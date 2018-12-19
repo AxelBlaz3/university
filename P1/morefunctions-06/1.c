7841c8008c0e36ef17f0f98bf0871267a5420f41
MiguelNdeCarvalho <miguel@cypheros.co>
2018-12-19 14:49:59 +0000

    fully rebase -Changed all of the folders names -Reoganized the files again -Translated one exercise

--- P1/morefunctions-06/1.c
+++ P1/morefunctions-06/1.c
@@ -21,16 +21,16 @@ int max(int n1, int n2, int n3)
 int main()
 {
   int n1, n2, n3;
-  printf ("Insira o primeiro numero:");
+  printf ("Insert the first number:");
   scanf("%d", &n1);
   //printf("%d", n1); // Just used to debug
-  printf ("Insira o segundo numero:");
+  printf ("Insert the second number:");
   scanf("%d", &n2);
   //printf("%d", n2); // Just used to debug
-  printf ("Insira o terceiro numero:");
+  printf ("Insert the third number:");
   scanf("%d", &n3);
   //printf("%d", n3); // Just used to debug
-  printf ("O maior numero e:");
+  printf ("The bigger number is:");
   printf (max (n1,n2,n3));
   return 0;
 }
